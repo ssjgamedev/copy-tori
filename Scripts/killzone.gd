abd40d5015e74612851c251d54d14e6ee9f9ff4e
ssjgamedev <ssjdevloperz@gmail.com>
2024-06-15 23:42:34 -0700

    Built first level

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -2,10 +2,19 @@ extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
+
 @onready var timer = $Timer
 
 
 
 func _on_timer_timeout():
+	
 	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
+
+
+func _on_body_entered(body):
+	
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
+	timer.start()
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,13 +6,15 @@ const JUMP_VELOCITY = 0
 @onready var animatedSprite = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = 100#ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		velocity.x = 0
+		
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -25,7 +27,7 @@ func _physics_process(delta):
 		animatedSprite.flip_h = true
 	elif direction > 0:
 		animatedSprite.flip_h = false
-	if direction:
+	if direction && is_on_floor():
 		velocity.x = direction * SPEED
 		
 	else:
