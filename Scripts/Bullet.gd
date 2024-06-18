93303c1b8fdb65bcf721727e4808db24ac655c4c
ssjgamedev <ssjdevloperz@gmail.com>
2024-06-17 22:36:17 -0700

    Working on Bullets

---
+++ Scripts/Bullet.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+const SPEED = 750
+
+func start(_position, _direction):
+	rotation = _direction
+	position = _position
+	velocity = Vector2(SPEED, 0).rotated(rotation)
+	
+
+
+func _physics_process(delta):
+	var collision = move_and_collide(velocity * delta)
+	
+	if collision:
+			velocity = velocity.bounce(collision.get_normal())
+			if collision.get_collider().has_method("hit"):
+				print("I am Hit")
+				collision.get_collider().hit()
+
+
+
+
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,6 +8,10 @@ const JUMP_VELOCITY = -300
 const gravityCONSTANT = 40
 @onready var animatedSprite = $AnimatedSprite2D
 var isTouchingLadder = false
+var Bullet = preload("res://Scenes/Bullet.tscn")
+var BulletSpeed = 200
+var direction = 0
+var facingLeft = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = gravityCONSTANT #ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -15,7 +19,6 @@ var gravity = gravityCONSTANT #ProjectSettings.get_setting("physics/2d/default_g
 #going to set gravity to zero when pressing up or down on ladder area
 func _physics_process(delta):
 	# Add the gravity.
-	
 	if !is_on_floor():
 		velocity.y += gravity * delta
 		#velocity.x = 0
@@ -44,16 +47,21 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	direction = Input.get_axis("ui_left", "ui_right")
 	if direction < 0:
 		animatedSprite.flip_h = true
+		facingLeft = true
 	elif direction > 0:
 		animatedSprite.flip_h = false
+		facingLeft = false
 	if direction && is_on_floor() || direction && isTouchingLadder:
 		velocity.x = direction * SPEED
 		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		if Input.is_action_just_pressed("shoot"):
+			shoot()
 
 	move_and_slide()
 
@@ -64,3 +72,12 @@ func updateLadderPhysics():
 	velocity.y = 0
 	gravity = 0
 	
+	
+func shoot():
+	var bulletProjectile = Bullet.instantiate()
+	if facingLeft:
+		print("I should be shooting left")
+		bulletProjectile.start($Marker.global_position, deg_to_rad(180))
+	else:
+		bulletProjectile.start($Marker.global_position, rotation)
+	get_tree().root.add_child(bulletProjectile)
